fcf495c8b28213ca5b45c29e11ea33475c50be90
Matthias Puech <matthias.puech@gmail.com>
2024-08-22 15:53:07 +0200

    CoreAudio: work around dropouts with combined devices
    ... by doubling latency.
    As advised by Fabian R-G, waiting for an upstream solution.

--- modules/juce_audio_devices/native/juce_CoreAudio_mac.cpp
+++ modules/juce_audio_devices/native/juce_CoreAudio_mac.cpp
@@ -1656,7 +1656,7 @@ public:
 
         currentSampleRate = sampleRate;
         currentBufferSize = bufferSize;
-        targetLatency = bufferSize;
+        targetLatency = bufferSize * 2;
 
         for (auto& d : getDeviceWrappers())
         {
